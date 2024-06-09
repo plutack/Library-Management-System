d4ccb7263eb9fcaadf701b9dc2ca45f929182b73
Talut salako <salakotalut@gmail.com>
2024-06-09 03:42:08 +0000

    fix: jsdoc comments

--- classes/Book.js
+++ classes/Book.js
@@ -1,25 +1,38 @@
 import uniqid from "uniqid";
+/**
+ * class to create a book object
+ */
 class Book {
+  /**
+   *@property {string} id
+   *@property {boolean} borrowed
+   */
   id = uniqid();
-  #borrowed = false;
-  constructor(title, author, isbn) {
-    // this.id =
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
+  borrowed = false;
+  /**
+   *@param {object} bookInfo
+   *@param {string} bookInfo.title
+   *@param {string} bookInfo.author
+   *@param {string} bookInfo.isbn
+   */
+  constructor(bookInfo) {
+    /**
+     *@property {string} title
+     *@property {string} author
+     *@property {string} isbn
+     */
+    this.title = bookInfo.title;
+    this.author = bookInfo.author;
+    this.isbn = bookInfo.isbn;
   }
 
   // methods
+  /**
+   *@property {function} isBorrowed
+   *@returns {boolean}
+   */
   isBorrowed() {
-    return this.#borrowed;
-  }
-
-  changeBorrowState() {
-    if (this.#borrowed) {
-      this.#borrowed = false;
-    } else {
-      this.#borrowed = true;
-    }
+    return this.borrowed;
   }
 }
 
